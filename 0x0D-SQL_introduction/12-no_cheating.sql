7967d3424f3be22bc76e595a7bd2c6e926e608fb
TosineBadges <tosinebadges@gmail.com>
2023-08-15 13:38:08 -0700

    12-no_cheating.sql completed

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,4 @@
-- A script that updates the score of Bob to 10 in the table second_table.
--- You are not allowed to use Bob’s id value, only the name field
--- The database name will be passed as an argument of mysql command
-
-UPDATE second_table SET score=10 WHERE name="Bob";
+-- Updates score of Bob to 10 in the table second_table.
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";
